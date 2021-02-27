098413ca242f14df487fec8fd172717c95761c13
VitorNoro <vmnoro@gmail.com>
2021-02-27 08:31:42 +0000

    Initial Player scene with collisions fixed

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,14 +1,10 @@
-extends Area2D
+extends KinematicBody2D
 
 export var speed = 200
 var screen_size
-var is_interacting
-
-signal talk
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	is_interacting = false
 
 func _process(delta):
 	var velocity = Vector2()
@@ -21,14 +17,12 @@ func _process(delta):
 	else:
 		$AnimatedSprite.stop()
 	
-	position += velocity * delta
+	var motion = velocity * delta
+	move_and_collide(motion)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
-	check_interaction()
-
 	#set_animation(velocity)
-	
 
 func check_movement(velocity : Vector2):
 	if Input.is_action_pressed("ui_right"):
@@ -51,12 +45,3 @@ func set_animation(velocity : Vector2):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
-
-func check_interaction():
-	if Input.is_key_pressed(KEY_SPACE) && !is_interacting:
-		is_interacting = true
-		emit_signal("talk")
-
-
-func _on_Player_talk():
-	pass # Replace with function body.
