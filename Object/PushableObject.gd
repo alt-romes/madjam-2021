a96f2925220d29805bbc662ae2c2428dc0cdd16a
VitorNoro <vmnoro@gmail.com>
2021-02-27 15:22:34 +0000

    Created pushable objects and adapted Player to be able to push them

--- Object/PushableObject.gd
+++ Object/PushableObject.gd
@@ -1,16 +1,74 @@
-extends Node
+extends KinematicBody2D
+
+export var player_node_path : NodePath
+export var sprite_img : Texture
+
+onready var sprite_node = $Sprite
+onready var player_node = get_node(player_node_path)
+
+var screen_size
+
+var FACE_RIGHT = 0
+var FACE_LEFT = 1
+var FACE_UP = 2
+var FACE_DOWN = 3
+
+var velocity = Vector2()
+var speed = 200
+var push_distance = 24
+var travelled_distance = 0
+
+var is_moving = false
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _ready():	
+	screen_size = get_viewport_rect().size
+	sprite_node.texture = sprite_img
+	sprite_node.position = Vector2(sprite_img.get_width() / 2 * -1, sprite_img.get_height() / 2 * -1)
+	$CollisionShape2D.position = sprite_node.position
+	$CollisionShape2D.get_shape().extents = Vector2(sprite_img.get_width() / 2, sprite_img.get_height() / 2)
+	$Area2D/CollisionShape2D.position = sprite_node.position
+	$Area2D/CollisionShape2D.get_shape().extents = Vector2(sprite_img.get_width() / 2, sprite_img.get_height() / 2)
 
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_accept"):		
+		var trigger_area : Area2D  = player_node.get_node("TriggerArea")		
+		if trigger_area.overlaps_area($Area2D) and !is_moving:
+			travelled_distance = 0
+			is_moving = true
+			velocity = push(player_node.get_direction(), velocity)
+			
+	if is_moving and travelled_distance < push_distance:
+		move_object(velocity,delta)
+	elif is_moving and travelled_distance >= push_distance:
+		is_moving = false
+		travelled_distance = 0
+		velocity = Vector2(0,0)
+		
+	position.x = clamp(position.x, sprite_img.get_width(), screen_size.x)
+	position.y = clamp(position.y, sprite_img.get_height(), screen_size.y)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func push(direction, velocity):
+	match direction:
+		FACE_RIGHT:
+			velocity.x += 1
+		FACE_LEFT:
+			velocity.x -= 1
+		FACE_UP:
+			velocity.y -= 1
+		FACE_DOWN:
+			velocity.y += 1
+		_:
+			print("Invalid direction")
+				
+	return velocity
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func move_object(velocity,delta):
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	var motion = velocity * delta
+	move_and_collide(motion)
+	travelled_distance += speed * delta
--- Player/Player.gd
+++ Player/Player.gd
@@ -2,14 +2,33 @@ extends KinematicBody2D
 
 export var speed = 200
 var screen_size
+var direction = 2
+var frame
+
+var FACE_RIGHT = 0
+var FACE_LEFT = 1
+var FACE_UP = 2
+var FACE_DOWN = 3
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	frame = $AnimatedSprite.frames.get_frame("default", 0)
+	$TriggerArea/TriggerShape.position.x = 0
+	$TriggerArea/TriggerShape.position.y = frame.get_height() / 10 * -1
+	$CollisionShape2D.position = $AnimatedSprite.position
+	$CollisionShape2D.get_shape().extents = Vector2(frame.get_width() / 2, frame.get_height() / 2)
 
-func _process(delta):
+	$TriggerArea/TriggerShape.get_shape().extents = Vector2(frame.get_width() / 2, (frame.get_height() / 2))
+
+func get_direction():
+	return direction
+
+func _physics_process(delta):
 	var velocity = Vector2()
 	
 	velocity = check_movement(velocity)
+	
+	#rotation = dir
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -27,12 +46,44 @@ func _process(delta):
 func check_movement(velocity : Vector2):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = false
+		
+		$TriggerArea/TriggerShape.position.x = frame.get_width() / 10 * 1
+		$TriggerArea/TriggerShape.position.y = 0
+			
+		$TriggerArea/TriggerShape.get_shape().extents = Vector2(frame.get_width() / 2, (frame.get_height() / 2))
+		
+		direction = FACE_RIGHT
+		
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
+		$AnimatedSprite.flip_h = true
+		$TriggerArea/TriggerShape.position.x = frame.get_width() / 10 * -1
+		$TriggerArea/TriggerShape.position.y = 0
+		
+		$TriggerArea/TriggerShape.get_shape().extents = Vector2(frame.get_width() / 2, (frame.get_height() / 2))
+		
+		direction = FACE_LEFT
+		
+	elif Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
+
+		$TriggerArea/TriggerShape.position.x = 0 #frame.get_width() / 2 * -1
+		$TriggerArea/TriggerShape.position.y = frame.get_height() / 10 * -1
+		
+		$TriggerArea/TriggerShape.get_shape().extents = Vector2(frame.get_width() / 2, (frame.get_height() / 2))
+		
+		direction = FACE_UP
+		
+	elif Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		
+		$TriggerArea/TriggerShape.position.x = 0 #frame.get_width() / 2 * -1
+		$TriggerArea/TriggerShape.position.y = frame.get_height() / 10 * 1
+		
+		$TriggerArea/TriggerShape.get_shape().extents = Vector2(frame.get_width() / 2, (frame.get_height() / 2))
+		
+		direction = FACE_DOWN
 		
 	return velocity
 
