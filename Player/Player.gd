701d7488a49db25ea996484ef35a2d69c263971b
mrmav <mrmav@outlook.pt>
2021-02-27 14:58:11 +0000

    added an initial Level template; changed display settings

--- Player/Player.gd
+++ Player/Player.gd
@@ -38,7 +38,7 @@ func _process(delta):
 			item.pickable_obj_resource = GameState.carried_item
 			item.player_node_path = self.get_path()
 			
-			get_node("../../main").add_child(item)
+			get_tree().root.get_node("Level").get_node("YSort").get_node("WinEvaluator").add_child(item)
 			
 			GameState.carried_item = null
 			is_holding_item = false
